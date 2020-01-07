3af2bd5bfddca8e9d7e53447d6bd361888513b46
Paul O'Leary McCann <polm@dampfkraft.com>
2020-01-07 16:22:10 +0900

    Add metadata files

--- setup.py
+++ setup.py
@@ -11,10 +11,5 @@ setup(name='unidic',
       long_description_content_type="text/markdown",
       url="https://github.com/polm/unidic-py",
       packages=setuptools.find_packages(),
-      classifiers=[
-          "License :: OSI Approved :: MIT License",
-          "Natural Language :: Japanese",
-          ],
-      python_requires='>=3.5',
       package_data={'unidic': ['dicdir/*']}
       )
