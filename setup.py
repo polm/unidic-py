704a5e73854a461fd3370ee3e230b250e50e35fb
Paul O'Leary McCann <polm@dampfkraft.com>
2020-01-07 16:27:35 +0900

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 from distutils.core import setup
 
 setup(name='unidic', 
-      version='0.0.3',
+      version='0.0.4',
       author="Paul O'Leary McCann",
       author_email="polm@dampfkraft.com",
       description="UniDic packaged for Python",
