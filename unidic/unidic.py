972cad731fb910875f897fc363eb9c4b9d1f4430
Paul O'Leary McCann <polm@dampfkraft.com>
2020-04-07 20:59:36 +0900

    Fix typo

--- unidic/unidic.py
+++ unidic/unidic.py
@@ -17,7 +17,7 @@ DICDIR = os.path.join(_curdir, 'dicdir')
 VERSION = get_version(DICDIR)
 
 if VERSION == '0':
-    msg.fail("You imported unidic, but dicitionary data is not installed. "
+    msg.fail("You imported unidic, but dictionary data is not installed. "
             "Run this command to install the latest UniDic: "
             "    python -m unidic download",
             exits=1)
