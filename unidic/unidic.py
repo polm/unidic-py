196616fb376de6ef0de03e52a35cdb1ebbf29a07
Paul O'Leary McCann <polm@dampfkraft.com>
2020-04-07 18:38:47 +0900

    Add fail message when run without data

--- unidic/unidic.py
+++ unidic/unidic.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from wasabi import msg
 
 def get_version(dicdir):
     with open(dicdir + '/version') as vfile:
@@ -10,3 +11,9 @@ _curdir = os.path.dirname(__file__)
 # This will be used elsewhere to initialize the tagger
 DICDIR = os.path.join(_curdir, 'dicdir')
 VERSION = get_version(DICDIR)
+
+if VERSION == '0':
+    msg.fail("You imported unidic, but dicitionary data is not installed. "
+            "Run this command to install the latest UniDic: "
+            "    python -m unidic download",
+            exits=1)
