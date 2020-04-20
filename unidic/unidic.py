cd3801adf469d380d9427aa4ea406d6792c5f1e9
Paul O'Leary McCann <polm@dampfkraft.com>
2020-04-20 22:12:48 +0900

    Remove installation check
    This obviously won't work when the download command is run.

--- unidic/unidic.py
+++ unidic/unidic.py
@@ -16,8 +16,3 @@ _curdir = os.path.dirname(__file__)
 DICDIR = os.path.join(_curdir, 'dicdir')
 VERSION = get_version(DICDIR)
 
-if VERSION == '0':
-    msg.fail("You imported unidic, but dictionary data is not installed. "
-            "Run this command to install the latest UniDic: "
-            "    python -m unidic download",
-            exits=1)
